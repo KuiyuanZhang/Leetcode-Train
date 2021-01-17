5115929fee6935cfff6a8f841e9a10f1244d3f0b
KuiyuanZhang <kuiyuanzhang@126.com>
2021-01-17 23:52:18 +0800

    2021 1 17

--- 09.Others/Graph/c399_Evaluate_Division.py
+++ 09.Others/Graph/c399_Evaluate_Division.py
@@ -1,4 +1,4 @@
-链接：https://leetcode-cn.com/problems/evaluate-division
+"""链接：https://leetcode-cn.com/problems/evaluate-division
 给你一个变量对数组 equations 和一个实数值数组 values 作为已知条件，其中 equations[i] = [Ai, Bi] 和 values[i] 共同表示等式 Ai / Bi = values[i] 。每个 Ai 或 Bi 是一个表示单个变量的字符串。
 另有一些以数组 queries 表示的问题，其中 queries[j] = [Cj, Dj] 表示第 j 个问题，请你根据已知条件找出 Cj / Dj = ? 的结果作为答案。
 返回 所有问题的答案 。如果存在某个无法确定的答案，则用 -1.0 替代这个答案。
@@ -33,3 +33,4 @@ values.length == equations.length
 queries[i].length == 2
 1 <= Cj.length, Dj.length <= 5
 Ai, Bi, Cj, Dj 由小写英文字母与数字组成
+"""
